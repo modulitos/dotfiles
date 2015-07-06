8816a4648d29de7c41c35e9b0250afad2bb7a654
Luke Swart <lukeswart@gmail.com>
2015-07-05 23:06:28 -0700

    Add new scripts, update old scripts

--- bin/resizepics.sh
+++ bin/resizepics.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 # Iterates over all files and resizes them
 
+# ex:
+# resizepics.sh "0616152002.jpg 0616152002a.jpg" 500
 for f in $1
 do
   echo "Resizing $f to a scaled width of $2 pixels."
