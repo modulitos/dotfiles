17679ca4821cf1e5e7994631239d5f9c7ee30629
Luke Swart <luke@lukeswart.net>
2016-08-07 05:33:51 -0700

    Rename backup scripts

--- bin/backup-home.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# -S is added to more efficiently handle Docker images
-# sudo does not affect the backup file ownership!
-sudo rsync -SaAXvx --info=progress2 --delete -H /home/lucas /run/media/lucas/lucas-backup/
-
