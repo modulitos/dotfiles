4a5a86ef82137d7b5d2d0a037fc4db69682f470e
modulitos <you@example.com>
2019-01-11 06:28:56 -0800

    use notify-send for timer, update airvpn script

--- bin/timer.sh
+++ bin/timer.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+message=$2
 countdown() {
   date1=$((`date +%s` + $1));
   while [ "$date1" -ne `date +%s`  ]; do
     echo -ne "$(date -u --date @$(($date1 - `date +%s`)) +%H:%M:%S)\r";
     sleep 0.1
   done
+  notify-send "time is up!" "$message"
   play ~/music/sounds/bell-ringing-04.wav
 }
 countdown $1
-message=$2
 echo "$message"
