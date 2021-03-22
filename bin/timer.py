92dc1cac9fbbbf20bb5a1ea1b949fd3ea284d558
modulitos <you@example.com>
2021-03-21 23:47:18 -0700

    fix timer

--- bin/timer.py
+++ bin/timer.py
@@ -3,7 +3,7 @@ import os
 import time
 import subprocess
 from pathlib import Path
-
+from sys import platform
 
 def timer(seconds: int, message: str):
     message = message if message else "time is up!"
@@ -18,18 +18,20 @@ def timer(seconds: int, message: str):
     print(f"message: {message}")
     home = str(Path.home())
 
-    # if macos:
-    subprocess.run(
-        ["osascript", "-e", f'display notification "" with title "{message}"']
-    )
-    bell_path = os.path.join(home, "sounds/bell-ringing-04.wav")
-    subprocess.run(["afplay", bell_path])
-
-
-# TODO: if linux:
-# notify-send "Time is up!" "$message"
-# subprocess.run(
-# ["notify-send", '"Time is up!"', f'{message}']
-# )
-#  subprocess.run(["play", bell_path])
-# play ~/sounds/bell-ringing-04.wav
+    if platform == "linux" or platform == "linux2":
+        # linux
+        # notify-send "Time is up!" "$message"
+        subprocess.run(
+            ["notify-send", '"Time is up!"', f'{message}']
+        )
+        bell_path = os.path.join(home, "music/sounds/bell-ringing-04.wav")
+        # play ~/music/sounds/bell-ringing-04.wav
+        subprocess.run(["play", bell_path])
+    elif platform == "darwin":
+        # OS X
+        # if macos:
+        subprocess.run(
+            ["osascript", "-e", f'display notification "" with title "{message}"']
+         )
+        bell_path = os.path.join(home, "sounds/bell-ringing-04.wav")
+        subprocess.run(["afplay", bell_path])
