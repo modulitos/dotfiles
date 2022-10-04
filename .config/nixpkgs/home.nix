723423f97809e43776ee275940edd85c8036c84c
modulitos <you@example.com>
2022-10-03 22:57:48 -0700

    updates

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -18,4 +18,43 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  home.packages = with pkgs; [
+    # vim
+    # curl
+    # zsh
+    # git
+    # slack
+    # ripgrep
+    # kubectl
+    # kustomize
+    # istioctl
+    jsonnet
+    jsonnet-bundler
+    postgresql
+    # cloud-sql-proxy
+    jq
+    lsof
+    # rename
+    # shellcheck
+    # yamllint
+    # go_1_18
+    # sublime4
+    # kubernetes-helm #Helm 3
+    # pgcenter
+    # keepass
+    # ruby_2_7
+  ];
+
+  # home.activation = {
+  #   linkDesktopApplications = {
+  #     after = [ "writeBoundary" "createXdgUserDirectories" ];
+  #     before = [ ];
+  #     data = ''
+  #       rm -rf ${config.xdg.dataHome}/"applications/home-manager"
+  #       mkdir -p ${config.xdg.dataHome}/"applications/home-manager"
+  #       cp -Lr ${config.home.homeDirectory}/.nix-profile/share/applications/* ${config.xdg.dataHome}/"applications/home-manager/"
+  #     '';
+  #   };
+  # };
 }
