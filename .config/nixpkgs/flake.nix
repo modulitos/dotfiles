0cfd286c48dd3889d29cec2737ee74e026959834
modulitos <modulitos@users.noreply.github.com>
2024-02-16 17:25:30 -0800

    updates

--- .config/nixpkgs/flake.nix
+++ .config/nixpkgs/flake.nix
@@ -7,8 +7,6 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  # helpful:
-  # https://github.com/chrisportela/dotfiles/blob/main/flake.nix
   outputs = { self, nixpkgs, home-manager }:
     let
       system = "x86_64-linux";
