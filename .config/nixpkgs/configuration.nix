f28373b52c74bfe70c17dea98e619c5ed864a8b2
modulitos <modulitos@users.noreply.github.com>
2024-04-13 22:35:13 -0700

    updates

--- .config/nixpkgs/configuration.nix
+++ .config/nixpkgs/configuration.nix
@@ -65,10 +65,10 @@
 
   # https://nixos.org/manual/nix/stable/command-ref/conf-file.html#conf-experimental-features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  nix.settings.trusted-users = [ "root" "@wheel" ];
   nix.settings = {
-    substituters = [ "https://devenv.cachix.org" ];
-    trusted-public-keys =
+    extra-trusted-users = [ "@wheel" ];
+    extra-substituters = [ "https://devenv.cachix.org" ];
+    extra-trusted-public-keys =
       [ "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=" ];
   };
 
